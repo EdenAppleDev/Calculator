18e8f039bbefea4eb08dc8561d0ab6b9a2c1edee
eden <noedenkim@gmail.com>
2025-05-27 20:17:34 +0900

    chore: 주석 추가

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,7 +1,9 @@
 class Calculator {
     
+    // 3개의 매개변수를 사용하고 Int로 반환
     func calculate(op: String, firstNumber: Int, secondNumber: Int) -> Int{
         
+        //switch 문으로 op값에 맞게 계산
         switch op {
         case "+":
             return firstNumber + secondNumber
@@ -13,13 +15,18 @@ class Calculator {
             return firstNumber / secondNumber
         default:
             return 0
+            //특수상황에 예외처리
         }
         
     }
     
 }
 
+
+//인스턴스 생성
 let calculator = Calculator()
+
+//인스턴스.메서드(매개변수)를 활용해 출력
 var addResult = calculator.calculate(op: "+", firstNumber: 4, secondNumber: 3)
 print(addResult)
 
