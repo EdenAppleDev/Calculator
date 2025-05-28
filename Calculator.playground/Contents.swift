48d58df6b12cb32379408abf477e952795627042
eden <noedenkim@gmail.com>
2025-05-28 20:55:10 +0900

    chore: 구조변경 (프로토컬, 클래스, 메소드 사용)

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,35 +1,57 @@
-class Calculator {
-    
-    // 3개의 매개변수를 사용하고 Int로 반환
-    func calculate(op: String, firstNumber: Double, secondNumber: Double) -> Double{
-        
-        //switch 문으로 op값에 맞게 계산
-        switch op {
-        case "+":
-            return firstNumber + secondNumber
-        case "-":
-            return firstNumber - secondNumber
-        case "*":
-            return firstNumber * secondNumber
-        case "/":
-            return firstNumber / secondNumber
-        case "%":
-            return firstNumber.truncatingRemainder(dividingBy: secondNumber)
-        default:
-            return 0
-            //특수상황에 예외처리
-        }
-        
+protocol ExecuteOperation {
+    func execute(_ a: Double, _ b: Double) -> Double
+}
+
+class AddOperation: ExecuteOperation {
+    func execute(_ a: Double, _ b: Double) -> Double {
+        return a + b
+    }
+}
+
+class SubtractOperation: ExecuteOperation {
+    func execute(_ a: Double, _ b: Double) -> Double {
+        return a - b
     }
     
 }
 
-//인스턴스 생성
-let calculator = Calculator()
+class MultiplyOperation: ExecuteOperation {
+    func execute(_ a: Double, _ b: Double) -> Double {
+        return a * b
+    }
+}
 
-//인스턴스.메서드(매개변수)를 활용해 출력
-var addResult = calculator.calculate(op: "%", firstNumber: 8, secondNumber: 3)
-print(addResult)
+class DivideOperation: ExecuteOperation {
+    func execute(_ a: Double, _ b: Double) -> Double {
+        return a / b
+    }
+}
+
+class Calculator {
+    private var op: ExecuteOperation
+    
+    init(op: ExecuteOperation) {
+        self.op = op
+    }
+    
+    func calculate(_ a: Double, _ b: Double) -> Double {
+        return op.execute(a, b)
+    }
+}
+
+let addCalc = Calculator(op: AddOperation())
+addCalc.calculate(4, 5)
+
+let minusCalc = Calculator(op: SubtractOperation())
+minusCalc.calculate(4, 5)
+minusCalc.calculate(10, 3)
+
+let multiplyCalc = Calculator(op: MultiplyOperation())
+multiplyCalc.calculate(4, 5)
+
+let divideCalc = Calculator(op: DivideOperation())
+divideCalc.calculate(4, 5)
+
 
 
 
